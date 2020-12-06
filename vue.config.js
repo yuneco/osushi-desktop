409d37a022f8713b6a6ed5eeeddcbde7f944bd67
yuneco <info@nekobooks.com>
2020-12-06 20:45:25 +0900

    公開用のおすしをビルドしました

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-    },
-  },
-};
+      builderOptions: {
+        appId: 'com.nekobooks.sushidesk',
+        mac: {
+          target: 'dmg',
+          icon: 'src/assets/app.icns'
+        },
+        win: {
+          target: 'portable',
+          icon: 'src/assets/app.ico'
+        }
+      }
+    }
+  }
+}
